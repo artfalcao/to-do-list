0ba34fc937f6dbd2e326ae31f030daf2d0ec7cd7
Arthur-Falcao <arthurmonteirofalcao1995@gmail.com>
2023-02-06 18:42:47 -0300

    criando tipos e entidades

---
+++ src/entities/Activity.ts
@@ -0,0 +1,38 @@
+import { Options } from "../types/selectProps";
+
+
+export default class Activity {
+    public _id : number;
+    public _description : string;
+    public _deadline : string;
+    public _priority : Options;
+    public _isCompleted : boolean;
+
+    constructor(description : string, deadline: string, priority : Options) {
+        this._description = description;
+        this._deadline = deadline;
+        this._priority = priority;
+        this._isCompleted = false;
+        this._id = Math.floor(Math.random() * 10000);
+    }
+
+    get description() : string {
+        return this._description;
+    }
+
+    get deadline() : string {
+        return this._deadline;
+    }
+
+    get priority() : Options {
+        return this._priority;
+    }
+
+    get isCompleted() : boolean {
+        return this._isCompleted;
+    }
+
+    get id() : number {
+        return this._id;
+    }
+}
---
+++ src/types/inputProps.ts
@@ -0,0 +1,10 @@
+import React, { InputHTMLAttributes } from "react";
+
+export type inputProps = {
+    type : string,
+    text : string,
+    name : string,
+    placeholder ?: string,
+    handleOnChange ?: (e: React.ChangeEvent<HTMLInputElement>) => void,
+    value : string  
+}
