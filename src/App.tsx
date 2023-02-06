af17072b2b176d00f5562a1fcba2f20237f994f6
Arthur-Falcao <arthurmonteirofalcao1995@gmail.com>
2023-02-06 18:40:14 -0300

    Criando rotas

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+
+// Pages
+import Home from './routes/Home';
+import NewActivity from './routes/NewActivity';
+import Activities from './routes/Activities';
+import ActivityPage from './routes/ActivityPage';
+
+// Components
+import Header from './components/Header';
+import Wrapper from './components/Wrapper';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <Router>
+      <Header />
+
+      <Wrapper>
+
+        <Routes>
+          <Route path='/' element={<Home />} />
+
+          <Route path='/newactivity' element={<NewActivity />} />
+
+          <Route path='/activities' element={<Activities />} />
+
+          <Route path='/activities/:id' element={<ActivityPage />} />
+
+        </Routes>
+        </Wrapper>
+
+        <Footer />
+
+    </Router>
+  );
+}
+
+export default App;
